20216dc8f50c47302bf5d750375d4d340ec02ff6
Hindaka Ilman Nafian <hindakailmannafian@gmail.com>
2012-05-22 03:37:39 +0700

    Final Coloring,keyboard and mouse function

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <gl\glut.h>
 #include <math.h>
 
-static int putarx=0 ,putary=0;
+static int putarx=0 ,putary=0, spin=0;
 
 void sate()
 {
@@ -13,16 +13,19 @@ void sate()
     glScalef(0.5,0.5,0.5);
     glRotatef((GLfloat)putarx,0.0,1.0,0.0);
     glRotatef((GLfloat)putary,0.0,0.0,1.0);
+    glRotatef(spin,1.0,0.0,0.0);
 
         glPushMatrix();
         //glTranslatef(0.0,0.0,0.0);
         glScalef(10.0,0.5,0.5);
+        glColor3f(0.3,0.2,0.1);
         glutSolidCube(0.25);
         glPopMatrix();
 
         //ball 1
         glPushMatrix();
         glTranslatef(0.0,0.0,0.0);
+        glColor3f(0.5,0.2,0.1);
         glutSolidSphere(0.25,20.0,16.0);
         glPopMatrix();
 
@@ -42,7 +45,7 @@ void sate()
 
 }
 
-void spin(int key, int x, int y){
+void mykey(int key, int x, int y){
 switch(key){
     case GLUT_KEY_HOME      : putarx = (putarx + 5) % 360;
                               glutPostRedisplay();
@@ -60,6 +63,24 @@ switch(key){
     }
 }
 
+void spinDisplay(void){
+    spin=spin+1.0;
+    if (spin > 360.0);
+    glutPostRedisplay();
+}
+
+void mouse(int button, int state, int x, int y){
+switch(button){
+    case GLUT_LEFT_BUTTON : if (state == GLUT_DOWN)
+                                glutIdleFunc(spinDisplay);
+                                break;
+    case GLUT_RIGHT_BUTTON : if(state == GLUT_DOWN)
+                                glutIdleFunc(NULL);
+                                break;
+    default                 : break;
+    }
+}
+
 void setMaterial()
 {
     //set properties of surfaces material
@@ -67,6 +88,7 @@ void setMaterial()
     GLfloat mat_diffuse[] = {0.6f,0.6f,0.6f,1.0f};
     GLfloat mat_specular[] = {1.0f,1.0f,1.0f,1.0f};
     GLfloat mat_shininess[] = {50.0f};
+    glColorMaterial(GL_FRONT_AND_BACK,GL_EMISSION);
     glMaterialfv(GL_FRONT,GL_AMBIENT,mat_ambient);
     glMaterialfv(GL_FRONT,GL_DIFFUSE,mat_diffuse);
     glMaterialfv(GL_FRONT,GL_SPECULAR,mat_specular);
@@ -123,12 +145,14 @@ int main(int argc, char **argv)
     glutInitWindowPosition(250,200);
     glutCreateWindow("bangun 3D");
     glutDisplayFunc(displayObject);//fungsi dari display object yang menggabungkan objek sate lighting material dan kamera
+    glEnable(GL_COLOR_MATERIAL);
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
     glShadeModel(GL_SMOOTH);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_NORMALIZE);
-    glutSpecialFunc(spin);
+    glutSpecialFunc(mykey);
+    glutMouseFunc(mouse);
     glClearColor(1.0f,1.0f,1.0f,0.0f);
     glViewport(0,0,640,480);
     glutMainLoop();
